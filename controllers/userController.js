85df37f8b2f7e48fa30008ef0220c12ea8608ab1
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2022-02-11 22:36:48 -0800

    More user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,6 +22,24 @@ module.exports = {
         return res.status(500).json(err);
       });
   },
-
+  // Get a single user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      // .populate('thoughts')
+      // .populate('friends')
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // Create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err))
+  }
 
 }
